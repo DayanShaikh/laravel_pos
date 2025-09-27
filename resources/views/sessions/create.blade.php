eeb6d4b7a364645dcf0f731eb344e172cea8b591
DayanShaikh <129284531+DayanShaikh@users.noreply.github.com>
2025-09-27 16:59:14 +0500

    changes

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -16,33 +16,12 @@
                     <div class="col-lg-4 col-md-8 col-12 mx-auto">
                         <div class="card z-index-0 fadeIn3 fadeInBottom">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 text-center">
+                                <div class="bg-gradient-info shadow-info border-radius-lg py-3 pe-1 text-center">
                                     @if(!empty(\App\Utility::setting("login_logo")))
                                     <span class="text-center"><img src="{{ Storage::url(\App\Utility::setting("login_logo"))}}" alt="Admin Logo"></span>
                                     @else
                                     <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">{!!\App\Utility::setting("site_title")!!}</h4>
                                     @endif
-                                    {{-- <div class="row mt-3">
-                                            <h6 class='text-white text-center'>
-                                                <span class="font-weight-normal">Email:</span> admin@material.com
-                                                <br>
-                                                <span class="font-weight-normal">Password:</span> secret</h6>
-                                            <div class="col-2 text-center ms-auto">
-                                                <a class="btn btn-link px-3" href="javascript:;">
-                                                    <i class="fa fa-facebook text-white text-lg"></i>
-                                                </a>
-                                            </div>
-                                            <div class="col-2 text-center px-1">
-                                                <a class="btn btn-link px-3" href="javascript:;">
-                                                    <i class="fa fa-github text-white text-lg"></i>
-                                                </a>
-                                            </div>
-                                            <div class="col-2 text-center me-auto">
-                                                <a class="btn btn-link px-3" href="javascript:;">
-                                                    <i class="fa fa-google text-white text-lg"></i>
-                                                </a>
-                                            </div>
-                                        </div> --}}
                                 </div>
                             </div>
                             <div class="card-body">
@@ -78,15 +57,15 @@
                                     <p class='text-danger inputerror'>{{ $message }} </p>
                                     @enderror
                                     <div class="text-center">
-                                        <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
+                                        <button type="submit" class="btn bg-gradient-info w-100 my-4 mb-2">Sign in</button>
                                     </div>
                                     {{-- <p class="mt-4 text-sm text-center">
                                         Don't have an account?
-                                        <a href="{{ route('register') }}" class="text-primary text-gradient font-weight-bold">Sign up</a>
+                                        <a href="{{ route('register') }}" class="text-info text-gradient font-weight-bold">Sign up</a>
                                     </p> --}}
                                     <p class="text-sm text-center">
                                         Forgot your password? Reset your password
-                                        <a href="{{ route('verify') }}" class="text-primary text-gradient font-weight-bold">here</a>
+                                        <a href="{{ route('verify') }}" class="text-info text-gradient font-weight-bold">here</a>
                                     </p>
                                 </form>
                             </div>
