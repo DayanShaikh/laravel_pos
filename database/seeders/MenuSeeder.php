b32ab0cb3b31af870eabb41865e9273b3d957226
DayanShaikh <129284531+DayanShaikh@users.noreply.github.com>
2025-11-24 23:55:02 +0500

    changes

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -23,7 +23,7 @@ class MenuSeeder extends Seeder
         ]);
         Menu::create([
             'title' => 'Items',
-            'url' =>'/item',
+            'url' =>'laravel_pos/public/item',
             'parent_id' => $Items1->id,
             'small_icon' =>'<i class="fa fa-tag"></i>',
             'icon' =>'',
@@ -37,21 +37,21 @@ class MenuSeeder extends Seeder
         ]);
         Menu::create([
             'title' => 'Purchase',
-            'url' =>'/purchase?is_return=0',
+            'url' =>'laravel_pos/public/purchase?is_return=0',
             'parent_id' => $invetory->id,
             'small_icon' =>'<i class="fa fa-cart-shopping"></i>',
             'icon' =>'',
         ]);
         Menu::create([
             'title' => 'Supplier',
-            'url' =>'/supplier',
+            'url' =>'laravel_pos/public/supplier',
             'parent_id' => $invetory->id,
             'small_icon' =>'<i class="fa fa-cart-flatbed"></i>',
             'icon' =>'',
         ]);
         Menu::create([
             'title' => 'Supplier Payments',
-            'url' =>'/supplier_payment',
+            'url' =>'laravel_pos/public/supplier_payment',
             'parent_id' => $invetory->id,
             'small_icon' =>'<i class="fa fa-cart-flatbed"></i>',
             'icon' =>'',
@@ -65,21 +65,21 @@ class MenuSeeder extends Seeder
         ]);
         Menu::create([
             'title' => 'Sales',
-            'url' =>'/sales',
+            'url' =>'laravel_pos/public/sales',
             'parent_id' => $Sales->id,
             'small_icon' =>'<i class="fa fa-sack-dollar"></i>',
             'icon' =>'',
         ]);
         Menu::create([
             'title' => 'Customer',
-            'url' =>'/customer',
+            'url' =>'laravel_pos/public/customer',
             'parent_id' => $Sales->id,
             'small_icon' =>'<i class="fa fa-cart-flatbed"></i>',
             'icon' =>'',
         ]);
         Menu::create([
             'title' => 'Customer Payment',
-            'url' =>'/customer_payment',
+            'url' =>'laravel_pos/public/customer_payment',
             'parent_id' => $Sales->id,
             'small_icon' =>'<i class="fa fa-cart-flatbed"></i>',
             'icon' =>'',
@@ -93,21 +93,21 @@ class MenuSeeder extends Seeder
         ]);
         Menu::create([
             'title' => 'Expense',
-            'url' =>'/expense_category',
+            'url' =>'laravel_pos/public/expense_category',
             'parent_id' => $Accounts->id,
             'small_icon' =>'<i class="fa fa-hand-holding-dollar"></i>',
             'icon' =>'',
         ]);
         Menu::create([
             'title' => 'Manage Expense',
-            'url' =>'/manage_expense',
+            'url' =>'laravel_pos/public/manage_expense',
             'parent_id' => $Accounts->id,
             'small_icon' =>'<i class="fa-solid fa-hand-holding-dollar"></i>',
             'icon' =>'',
         ]);
         Menu::create([
             'title' => 'Manage Account',
-            'url' =>'/account',
+            'url' =>'laravel_pos/public/account',
             'parent_id' => $Accounts->id,
             'small_icon' =>'<i class="fa-solid fa-hand-holding-dollar"></i>',
             'icon' =>'',
