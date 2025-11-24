e160f3cc1cefab1dea78d7891988896069bfded5
DayanShaikh <129284531+DayanShaikh@users.noreply.github.com>
2025-11-25 00:02:36 +0500

    changes

--- app/Utility.php
+++ app/Utility.php
@@ -15,13 +15,13 @@ class Utility
 						<div class="item">';
 		foreach ($menus as $menu) {
 			if ($menu->parent_id == null) {
-				$html .= '<a ' . (empty($menu->parent_id) ? 'href="' . $menu->url . '"' : '') . ' class="sub-btn">' . $menu->small_icon . $menu->title . (empty($menu->parent_id) ? '<i class="fas fa-angle-right dropdown"></i>' : '') . '</a>';
+				$html .= '<a ' . (empty($menu->parent_id) ? 'href="' . url($menu->url) . '"' : '') . ' class="sub-btn">' . $menu->small_icon . $menu->title . (empty($menu->parent_id) ? '<i class="fas fa-angle-right dropdown"></i>' : '') . '</a>';
 			}
 			if ($menu->parent_id) {
 				$sub_menu = Menu::where('parent_id', $menu->parent_id)->get();
 				$html .= '<div class="sub-menu">';
 				foreach ($sub_menu as $parent) {
-					$html .= '<a href="' . $parent->url . '" class="sub-item"> ' . $parent->small_icon . ' ' . $parent->title . ' </a>';
+					$html .= '<a href="' . url($parent->url) . '" class="sub-item"> ' . $parent->small_icon . ' ' . $parent->title . ' </a>';
 				}
 				$html .= '</div>';
 			}
