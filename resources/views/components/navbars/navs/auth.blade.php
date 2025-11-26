f86a95ea283f9876019529946dd30d6e01c922fb
DayanShaikh <129284531+DayanShaikh@users.noreply.github.com>
2025-11-26 13:54:46 +0500

    changes

--- resources/views/components/navbars/navs/auth.blade.php
+++ resources/views/components/navbars/navs/auth.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
         <div class="col-md-6"></div>
-        <div class="col-md-6">
+        <div class="col-md-6" style="padding: 0px 20px;">
             <div class="mt-2 mt-sm-0 me-md-0 me-sm-4 me-sm-4 d-contents" id="navbar">
                 <ul class="navbar-nav justify-content-end">
                     <li class="nav-item dropdown">
